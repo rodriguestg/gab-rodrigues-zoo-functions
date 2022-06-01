e5a273977e4d91736820a3e9b160a50a5ce6d86b
rodriguestg <grs.ibr@gmail.com>
2022-06-01 14:40:19 -0300

    Subindo Requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,11 +1,15 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
   // seu código aqui
-  // const { id, name } = data;
-  // return name;
+  if (ids.length === 0) {
+    return [];
+  }
+  if (ids.length === 1) {
+    return [species.find((specie) => specie.id === ids[0])];
+  }
+  return ids.map((specie) => species.find((specieId) => specieId.id === specie));
 }
-
-console.log(getSpeciesByIds());
 
 module.exports = getSpeciesByIds;
