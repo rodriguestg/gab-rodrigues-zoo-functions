872836adf4702b9207fde31339ece86cf5740153
rodriguestg <grs.ibr@gmail.com>
2022-06-01 01:00:03 -0300

    Subindo Requisito 16 e 13

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -21,25 +21,40 @@ describe('Testes da função getOpeningHours', () => {
   it('Teste se quarta a noite o Zoo está fechado', () => {
     expect(getOpeningHours('Wednesday', '09:00-PM')).toBe('The zoo is closed');
   });
-  it('Teste se dia Thu lança uma excesão', () => {
+  it('Teste se dia Thu lança uma exceção de erro', () => {
     function testeThu() {
       getOpeningHours('Thu', '09:00-AM');
     }
     expect(testeThu).toThrow('The day must be valid. Example: Monday');
-  });/*
-  it('Testes se segunda o Zoo está fechado', () => {
-    expect(getOpeningHours('Friday', '09:00-AM')).toBe('The zoo is closssed');
   });
-  it('Testes se segunda o Zoo está fechado', () => {
-    expect(getOpeningHours('Saturday', '09:00-AM')).toBe('The zosso is closed');
+  it('Teste se dia Friday/ZM lança uma exceção de erro', () => {
+    function testeZm() {
+      getOpeningHours('Friday', '09:00-ZM');
+    }
+    expect(testeZm).toThrow('The abbreviation must be \'AM\' or \'PM\'');
   });
-  it('Testes se segunda o Zoo está fechado', () => {
-    expect(getOpeningHours('Sunday', '09:00-AM')).toBe('The zoo issss closed');
-  });  
-  it('Testes se segunda o Zoo está fechado', () => {
-    expect(getOpeningHours('Monday', '09:00-AM')).toBe('The zoo is clssosed');
+  it('Teste se dia Saturday/C9 lança uma exceção de erro', () => {
+    function testeC9() {
+      getOpeningHours('Saturday', 'C9:00-AM');
+    }
+    expect(testeC9).toThrow('The hour should represent a number');
   });
-  it('Testes se segunda o Zoo está fechado', () => {
-    expect(getOpeningHours('Tuesday', '09:60-AM')).toBe('The minutes must be between 0 and 59');
-  }); */
+  it('Teste se dia Sunday/C0 lança uma exceção de erro', () => {
+    function testeC9() {
+      getOpeningHours('Sunday', '09:c0-AM');
+    }
+    expect(testeC9).toThrow('The minutes should represent a number');
+  });
+  it('Teste se dia Monday/13 lança uma exceção de erro', () => {
+    function testeC9() {
+      getOpeningHours('Monday', '13:00-AM');
+    }
+    expect(testeC9).toThrow('The hour must be between 0 and 12');
+  });
+  it('Teste se dia Tuesday/60 lança uma exceção de erro', () => {
+    function testeC9() {
+      getOpeningHours('Tuesday', '09:60-AM');
+    }
+    expect(testeC9).toThrow('The minutes must be between 0 and 59');
+  });
 });
