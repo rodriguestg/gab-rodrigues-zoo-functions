8194e2ccf3e5fd2b659166bee097c0d055215857
rodriguestg <grs.ibr@gmail.com>
2022-06-01 22:40:04 -0300

    Subindo Requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,6 +2,15 @@ const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (typeof employeeName === 'undefined') {
+    return {};
+  }
+  // const colaborador = data.employees.find((aName) => aName.firstName === employeeName);
+  // return colaborador;
+  const emName = employeeName;
+  const dados = data.employees;
+  const colab = dados.find((names) => (names.firstName === emName || names.lastName === emName));
+  return colab;
 }
 
 module.exports = getEmployeeByName;
