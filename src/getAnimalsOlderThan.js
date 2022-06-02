2b0a13095beb928b01293e1428b49217edb9d068
rodriguestg <grs.ibr@gmail.com>
2022-06-01 22:13:20 -0300

    Subindo Requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,6 +2,14 @@ const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const animalFilter = data.species.find((aName) => aName.name === animal);
+  const dadosAnimais = animalFilter.residents;
+  const idadeAnimal = dadosAnimais.map((animalIda) => animalIda.age);
+  const idadeCorreta = idadeAnimal.filter((correto) => (correto < age));
+  if (idadeCorreta.length === 0) {
+    return true;
+  }
+  return false;
 }
 
 module.exports = getAnimalsOlderThan;
