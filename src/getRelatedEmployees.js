aed9b4bef5870e5530d74e0478297388a67d6c5a
rodriguestg <grs.ibr@gmail.com>
2022-06-03 21:08:41 -0300

    Subindo Requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,20 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
 function isManager(id) {
   // seu código aqui
+  const usuario = employees.some((gerenteFiltrado) => gerenteFiltrado.managers.includes(id));
+  return usuario;
 }
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
+  if (isManager(managerId) === true) {
+    const equipe = employees.filter((equipeId) => equipeId.managers.includes(managerId));
+    const nomesEquipe = equipe.map((employ) => `${employ.firstName} ${employ.lastName}`);
+    return nomesEquipe;
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
